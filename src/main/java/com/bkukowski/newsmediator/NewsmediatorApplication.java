2b40a427fce06564701f27c15c19650117dead08
Bernard <bernard.burn@gmail.com>
2019-10-06 13:40:22 +0200

    project template
    project template generated by spring initializr

---
+++ src/main/java/com/bkukowski/newsmediator/NewsmediatorApplication.java
@@ -0,0 +1,13 @@
+package com.bkukowski.newsmediator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsmediatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsmediatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bkukowski/newsmediator/NewsmediatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bkukowski.newsmediator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewsmediatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
